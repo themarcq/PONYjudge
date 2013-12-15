a73c2fcb623fa5c0c783c66a295df1fe6a544d8d
themarcq <kontakt@marek-matys.pl>
2013-12-15 23:29:43 +0100

    Update index.php
    initial index template

--- www/index.php
+++ www/index.php
@@ -1,3 +1,27 @@
 <?
-echo 'index!'
+include('globals.php'); // global variables
+include('libs.php'); //libraries
+storage_connect(); // connect with storage - from lstorage.php
+storage_request(WEBSITECONFIG); // load website config - from lstorage.php
+auth(); // check if user is logged in - from lauth.php
+?>
+
+<html>
+<head>
+<title><? echo WEBSITETITLE; ?></title>
+</head>
+<body>
+
+<?
+template_show_header(); // from template.php
+template_show_menu();
+template_show_body();
+template_show_footer();
+?>
+
+</body>
+</html>
+
+<?
+storage_disconnect();
 ?>
