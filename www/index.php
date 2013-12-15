82736d336bce571197db307c29585a0d07a9a168
themarcq <kontakt@marek-matys.pl>
2013-12-15 23:59:47 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -4,13 +4,13 @@ $global= new global;
 include('libs.php'); //libraries
 $storage=new storage;
 $storage->connect($global); // connect with storage - from lstorage.php
-$global->takeconfig($storage->request($storage.globalwwwconfig)); // load website config - from lstorage.php
-$global->takeconfig($storage->request($storage.auth)); // check if user is logged in - from lauth.php
+$global->takeconfig($storage->request($storage->globalwwwconfig)); // load website config - from lstorage.php
+$global->takeconfig($storage->request($storage->auth)); // check if user is logged in - from lauth.php
 ?>
 
 <html>
 <head>
-<title><? echo global.websitetitle; ?></title>
+<title><? echo global->websitetitle; ?></title>
 </head>
 <body>
 
