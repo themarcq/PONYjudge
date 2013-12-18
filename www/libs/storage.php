5e4ba52b40a546bb082884da1bac5178f7172c7f
themarcq <kontakt@marek-matys.pl>
2013-12-18 18:47:37 +0100

    Update storage.php

--- www/libs/storage.php
+++ www/libs/storage.php
@@ -1,5 +1,15 @@
 <?
-class storage{
+
+class storage {
+
+    public function connect($G) {
+       return mysql_connect($G->storagehost,'root',$G->storagepassword);
+    }
+
+    public function disconnect(){
+        mysql_close();
+    }
 
 }
+
 ?>
