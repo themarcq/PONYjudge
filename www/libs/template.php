00ffc3027e5b6dce2a077c9a3b1ea3f127db789d
themarcq <kontakt@marek-matys.pl>
2013-12-18 18:48:01 +0100

    Update template.php

--- www/libs/template.php
+++ www/libs/template.php
@@ -1,5 +1,23 @@
 <?
-class template(){
 
+class template{
+
+    public function show_header(){
+        echo '<div>header</div>';
+    }
+    
+    public function show_menu(){
+        echo '<div>menu</div>';
+    }
+    
+    public function show_body(){
+        echo '<div>body</div>';
+    }
+    
+    public function show_footer(){
+        echo '<div>footer</div>';
+    }
+    
 }
+
 ?>
