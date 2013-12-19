bcfed84844ff3c708e7f88b600070290b8050704
themarcq <kontakt@marek-matys.pl>
2013-12-19 12:14:54 +0100

    Update template.php

--- www/libs/template.php
+++ www/libs/template.php
@@ -1,21 +1,33 @@
 <?
 
 class template{
-
-    public function show_header(){
-        echo '<div>header</div>';
+    public $errors;
+    public $errors_number=0;
+    public function show(){
+        global $globals;
+        echo'
+        <html>
+            <head>
+                <title>'.$globals->websitetitle.'</title>
+                <link href="styles/style.css" rel="stylesheet" type="text/css" />
+            </head>
+            <body>
+                <div id="head">head</div>
+                <div id="menu">menu</div>
+                <div id="container">
+       ';
+        foreach($this->errors as $error){
+            echo '<div id="error">'.$error.'</div>';
+        }    
+        echo'
+                </div>
+            </body>
+        </html
+        ';
     }
     
-    public function show_menu(){
-        echo '<div>menu</div>';
-    }
-    
-    public function show_body(){
-        echo '<div>body</div>';
-    }
-    
-    public function show_footer(){
-        echo '<div>footer</div>';
+    public function show_error($s){
+        $this->errors[$this->errors_number++]=$s;
     }
     
 }
