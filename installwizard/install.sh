de5e44614d4054af979e5f869827fed449c8e267
szymekrak <szymekr12@gmail.com>
2013-12-31 22:47:08 +0100

    Update install.sh

--- installwizard/install.sh
+++ installwizard/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #0.if there is no certificates - create them
 #1.what do you want to install here or regenerate certificates
+#1.5.create users and keys for magic ssh stuff
 #2.install it
