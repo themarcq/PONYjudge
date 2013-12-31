f7bf2728a43968936a66ab409d2f2b51106425f3
themarcq <themarcq@gmail.com>
2013-12-31 10:08:19 +0100

    install

---
+++ configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#compile everything in sources
+#copy it to destinated folders
+#create certificates
+#pack everything to .tar.gz
+tar cf PONYjudge.tar.gz www/ storage/ judge/ installwizard/*
