87cc392359464059262f96e04a9d097ecf9876d1
szymekrak <szymekr12@gmail.com>
2014-01-06 19:44:11 +0100

    lol

--- judge/run.sh
+++ judge/run.sh
@@ -3,9 +3,9 @@ source ./judge.conf
 timelimit=$4
 tmpfile="/tmp/$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"
 echo "sudo -u $JUDGEUSER bash $LIMITER $1 $2 $3"
-sudo -u $JUDGEUSER "bash $LIMITER $1 $2 $3" > $tmpfile &
+sudo -u $JUDGEUSER bash $LIMITER $1 $2 $3 > $tmpfile &
 pid=$!
-(sleep $timelimit;if [ ! "x`ps -e | grep $pid`" == "x" ]; then kill $pid; fi)&
+(sleep ${timelimit};if [ ! "x`ps -e | grep $pid`" == "x" ]; then kill $pid; fi)&
 wait $pid
 cat $tmpfile
 rm $tmpfile
