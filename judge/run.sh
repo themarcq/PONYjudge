cbddd7e1ff5141ab0ea17b649044eafebc2be27b
szymekrak <szymekr12@gmail.com>
2014-01-06 20:52:05 +0100

    lol2

--- judge/run.sh
+++ judge/run.sh
@@ -3,10 +3,11 @@ PATH=/usr/bin:/bin:/usr/sbin:/sbin
 source ./judge.conf
 timelimit=$4
 tmpfile="/tmp/$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"
-sudo -u $JUDGEUSER $LIMITER "$1" "$2" "$3" > $tmpfile &
+commandline="$LIMITER \"$1\" $2 $3"
+su $JUDGEUSER -s /bin/bash -c "${commandline}" > $tmpfile &
 pid=$!
-(sleep $timelimit;if [ ! "x`ps -e | grep $pid`" == "x" ]; then kill $pid;echo 'lol, I killed that fucker'; fi)&
-wait $pid
+(sleep $timelimit;if [ ! "x`ps -e | grep $pid`" == "x" ]; then kill -s SIGKILL $pid;echo 'lol, I killed that fucker'; fi)&
+wait $pid 2> /dev/null
 echo '<wynik>'
 cat $tmpfile
 echo '</wynik>'
