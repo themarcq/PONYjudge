464cf35b7dc76ece8478efa722ea8539369d67b0
themarcq <themarcq@gmail.com>
2014-02-01 15:41:49 +0100

    preparations for file handling in storage

--- storage/mysql.cpp
+++ storage/mysql.cpp
@@ -1,3 +1,8 @@
+/*
+this shit on ubuntu needs: libmysqlcppconn-dev
+compile by(on ubuntu ofc): g++ -Wall -I/usr/include/cppconn <my name> -L/usr/lib -lmysqlcppconn
+*/
+
 /* Standard C++ headers */
 #include <cstdio>
 #include <iostream>
@@ -5,12 +10,9 @@
 #include <memory>
 #include <string>
 #include <stdexcept>
+#include <map>
 
 /* MySQL Connector/C++ specific headers */
-/*
-this shit on ubuntu needs: libmysqlcppconn-dev
-compile by(on ubuntu ofc): g++ -Wall -I/usr/include/cppconn <my name> -L/usr/lib -lmysqlcppconn
-*/
 #include <driver.h>
 #include <connection.h>
 #include <statement.h>
@@ -20,90 +22,56 @@ compile by(on ubuntu ofc): g++ -Wall -I/usr/include/cppconn <my name> -L/usr/lib
 #include <resultset_metadata.h>
 #include <exception.h>
 #include <warning.h>
+
 #define COLNAME 200
 
 using namespace std;
 using namespace sql;
 
-string load_line(){
+//config variables
+string DBHOST;
+string USER;
+string PASSWORD;
+string DATABASE;
 
-    string s;
-    char c=getchar_unlocked();
-    while( c>=32 && c<=126 ){
-        s+=c;
-        c=getchar_unlocked();
-    }
-    return s;
+//configurations
+int load_config_file();
 
-}
+//metadata loading functions
+string load_line();
+string load_word();
 
-string load_word(){
+//mysql help functions
+static void retrieve_data_and_print (ResultSet *, int);
+bool are_strings_equal(string, string);
 
-    string s;
-    char c=getchar_unlocked();
-    while( c>=32 && c<=126 && c!=' ' ){
-        if(c==EOF)return "EOF";
-        s+=c;
-        c=getchar_unlocked();
-    }
-//    printf("|%s|",s.c_str());
-    return s;
-}
+//mysql main function
+void execute_mysql_request(Statement *);
 
-static void retrieve_data_and_print (ResultSet *rs, int colidx) {
+//file handling
+void load_file();
+void send_file();
 
-    /* retrieve the row count in the result set */
-    printf("NumberOfRows %d\n",(int)rs -> rowsCount());
-    while (rs->next()) {
-            cout << rs -> getString(colidx) << endl;
-    } // while
-
-    cout << endl;
-
-} // retrieve_data_and_print()
-
-bool are_strings_equal(string s1, string s2){
-//printf("comparing |%s| and |%s| ",s1.c_str(),s2.c_str());
-    if(s1.compare(s2)==0){
-//        printf("equal\n");
-        return true;
-    }
-    else{
-//        printf("not equal\n");
-        return false;
-    }
-    /*for(int i=0;s1[i]!=0 and s2[i]!=0;i++)
-        if(s1[i]!=s2[i])
-            return false;
-    return true;*/
-}
+//7zip handling
+void load_7z();
+void send_7z();
+void load_file_to_7z();
 
 int main(int argc, const char *argv[]) {
 
-
-    //to trza wczytać z pliku
-    char DBHOST[255]="tcp://127.0.0.1:3306";
-    char USER[255]="root";
-    char PASSWORD[255]="alleluja1";
-    char DATABASE[255]="lol";
-
+    if(load_config_file()==1) {
+        cout << "Error parsing file!";
+        return 1;
+    }
     Driver *driver;
     Connection *con;
     Statement *stmt;
-    ResultSet *res;
-    PreparedStatement *prep_stmt;
-
-    /* initiate url, user, password and database variables */
-    string url(argc >= 2 ? argv[1] : DBHOST);
-    const string user(argc >= 3 ? argv[2] : USER);
-    const string password(argc >= 4 ? argv[3] : PASSWORD);
-    const string database(argc >= 5 ? argv[4] : DATABASE);
 
     try {
         driver = get_driver_instance();
-        con = driver -> connect(url, user, password);
+        con = driver -> connect(DBHOST, USER, PASSWORD);
         con -> setAutoCommit(1);
-        con -> setSchema(database);
+        con -> setSchema(DATABASE);
         stmt = con -> createStatement();
 
         //retrieving queries
@@ -111,27 +79,32 @@ int main(int argc, const char *argv[]) {
         Request=load_word();
         while(!are_strings_equal(Request,"END")) {
             if(are_strings_equal(Request,"DATABASE")) {
-                string Query;
-                Query = load_line();
-                res = stmt -> executeQuery (Query.c_str());
-                retrieve_data_and_print (res, 1);
-                printf("EndOfResponse\n");
+                execute_mysql_request(stmt);
             } else if(are_strings_equal(Request,"FILE")) {
-                //check what is it ( take or send file )
-                //then take its name, size, md5sum, chmods(optionally) and whatever we want
-                //wait for characteristic beggining and load till reaching destinated size and characteristic end(we can chat if everything is ok)
-                //check hash, save if good and throw prompt
+                Request=load_word();
+                if(are_strings_equal(Request,"SEND")) {
+                    load_file();
+                } else if(are_strings_equal(Request,"TAKE")) {
+                    send_file();
+                } else {
+                    cout<<"ERROR - Wrong request";
+                }
             } else if(are_strings_equal(Request,"7Z")) {
-                //everything same as is in file
-                //but with adding subfiles to existing directories
-                //and we can skip compressing
+                Request=load_word();
+                if(are_strings_equal(Request,"SEND")) {
+                    load_7z();
+                } else if(are_strings_equal(Request,"TAKE")) {
+                    send_7z();
+                } else if(are_strings_equal(Request,"UPDATE")) {
+                    load_file_to_7z();
+                } else {
+                    cout<<"ERROR - Wrong request";
+                }
             } else {
                 cout<<"ERROR - Wrong request";
             }
             Request=load_word();
         }
-        
-        /* Clean up */
 
     } catch (SQLException &e) {
         cout << "ERROR: SQLException in " << __FILE__;
@@ -160,4 +133,121 @@ int main(int argc, const char *argv[]) {
     }
 
     return EXIT_SUCCESS;
-} // main()
+}
+
+int load_config_file() {
+
+    std::map < std::string, std::map < std::string, std::string > > vars;
+    FILE * pFile;
+    int c;
+    string actsection;
+    string actname;
+    string loading;
+    pFile=fopen ("config.ini","r");
+    if (pFile==NULL)
+        return 1;
+    else {
+        do {
+            c = getc (pFile);
+            if(c=='[') {
+                loading="";
+                c=getc(pFile);
+                while(c!=']') {
+                    loading+=c;
+                    c=getc(pFile);
+                }
+                actsection=loading;
+            } else if((c>='a' and c<='z') or (c>='A' and c<='Z')) {
+                loading="";
+                while(c!=' ' and c!='=') {
+                    loading+=c;
+                    c=getc(pFile);
+                }
+                actname=loading;
+                while(c==' ')c=getc(pFile);
+                loading="";
+                c=getc(pFile);
+                while(c!='\n' and c!=' ') {
+                    loading+=c;
+                    c=getc(pFile);
+                }
+                vars[actsection][actname]=loading;
+                //printf("[%s]|%s|=|%s|\n",actsection.c_str(),actname.c_str(),loading.c_str());
+            }
+        } while (c != EOF);
+        fclose (pFile);
+    }
+
+    DBHOST=vars["mysql"]["dbhost"];
+    USER=vars["mysql"]["user"];
+    PASSWORD=vars["mysql"]["password"];
+    DATABASE=vars["mysql"]["database"];
+
+    return 0;
+
+}
+
+string load_line() {
+
+    string s;
+    char c=getchar_unlocked();
+    while( c>=32 && c<=126 ) {
+        s+=c;
+        c=getchar_unlocked();
+    }
+    return s;
+
+}
+
+string load_word() {
+
+    string s;
+    char c=getchar_unlocked();
+    while( c>=32 && c<=126 && c!=' ' ) {
+        if(c==EOF)return "EOF";
+        s+=c;
+        c=getchar_unlocked();
+    }
+    return s;
+}
+
+static void retrieve_data_and_print (ResultSet *rs, int colidx) {
+
+    printf("NumberOfRows %d\n",(int)rs -> rowsCount());
+    while (rs->next()) {
+        cout << rs -> getString(colidx) << endl;
+    }
+
+    cout << endl;
+
+}
+
+bool are_strings_equal(string s1, string s2) {
+
+    if(s1.compare(s2)==0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void execute_mysql_request(Statement *stmt) {
+
+    string Query;
+    Query = load_line();
+    ResultSet *res;
+    res = stmt -> executeQuery (Query.c_str());
+    retrieve_data_and_print (res, 1);
+    printf("EndOfResponse\n");
+
+}
+
+//file handling
+void load_file() {}
+void send_file() {}
+
+//7zip handling
+void load_7z() {}
+void send_7z() {}
+void load_file_to_7z() {}
