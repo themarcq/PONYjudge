362ac0b6f940736b7f5f79a705a64578aeeaaa10
themarcq <themarcq@gmail.com>
2014-03-21 17:18:28 +0100

    initial

--- storage/storage.cpp
+++ storage/storage.cpp
@@ -1,19 +1,315 @@
-#include<iostream>
+/*
+this shit on ubuntu needs: libmysqlcppconn-dev
+compile by(on ubuntu ofc): g++ -Wall -I/usr/include/cppconn <my name> -L/usr/lib -lmysqlcppconn
+*/
+
+/* Standard C++ headers */
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <string>
+#include <stdexcept>
+#include <map>
+#include <stdlib.h>
+#include <time.h>
+
+/* MySQL Connector/C++ specific headers */
+#include <driver.h>
+#include <connection.h>
+#include <statement.h>
+#include <prepared_statement.h>
+#include <resultset.h>
+#include <metadata.h>
+#include <resultset_metadata.h>
+#include <exception.h>
+#include <warning.h>
+
+#define COLNAME 200
+#define _kurwa( (
+
 using namespace std;
+using namespace sql;
 
-main() {
+//config variables
+std::map < std::string, std::map < std::string, std::string > > CONFIG;
 
+//configurations
+int load_config_file();
+
+//metadata loading functions
+string load_line();
+string load_word();
+
+//helping functions
+static void retrieve_data_and_print (ResultSet *, int);
+bool are_strings_equal(string, string);
+string get_random_string(int);
+int save_bytes_to_file(int /*size*/, string /*path*/);
+void send_file_out(string /*path*/);
+
+//mysql main function
+void execute_mysql_request(Statement *);
+
+//file handling
+void download_file();
+void send_file();
+
+//7zip handling
+void download_7z();
+void send_7z();
+void load_file_to_7z();
+
+int main(int argc, const char *argv[]) {
+
+    if(load_config_file()==1) {
+        cout << "Error parsing file!";
+        return 1;
+    }
+
+
+    //retrieving queries
     string Request;
-    while(cin>>Request) {
-        if(Request.equals('DATABASE')) {
-//send rest of the request to mysql
-        } else if(Request.equals('FILE')) {
-//check what is it ( take or send file )
-        } else if(Request.equals('7Z')) {
-//7z requests
+    Request=load_word();
+    if(are_strings_equal(Request,"DATABASE")) {
+        execute_mysql_request();
+    } else if(are_strings_equal(Request,"FILE")) {
+        Request=load_word();
+        if(are_strings_equal(Request,"SEND")) {
+            download_file();
+        } else if(are_strings_equal(Request,"TAKE")) {
+            send_file();
         } else {
             cout<<"ERROR - Wrong request";
         }
+    } else if(are_strings_equal(Request,"7Z")) {
+        Request=load_word();
+        if(are_strings_equal(Request,"SEND")) {
+            download_7z();
+        } else if(are_strings_equal(Request,"TAKE")) {
+            send_7z();
+        } else if(are_strings_equal(Request,"UPDATE")) {
+            load_file_to_7z();
+        } else {
+            cout<<"ERROR - Wrong request";
+        }
+    } else {
+        cout<<"ERROR - Wrong request";
+    }
+    Request=load_word();
+}
+
+return EXIT_SUCCESS;
+
+}
+
+int load_config_file() {
+
+    FILE * pFile;
+    int c;
+    string actsection;
+    string actname;
+    string loading;
+    pFile=fopen ("config.ini","r");
+    if (pFile==NULL)
+        return 1;
+    else {
+        do {
+            c = getc (pFile);
+            if(c=='[') {
+                loading="";
+                c=getc(pFile);
+                while(c!=']') {
+                    loading+=c;
+                    c=getc(pFile);
+                }
+                actsection=loading;
+            } else if((c>='a' and c<='z') or (c>='A' and c<='Z')) {
+                loading="";
+                while(c!=' ' and c!='=') {
+                    loading+=c;
+                    c=getc(pFile);
+                }
+                actname=loading;
+                while(c==' ')c=getc(pFile);
+                loading="";
+                c=getc(pFile);
+                while(c!='\n' and c!=' ') {
+                    loading+=c;
+                    c=getc(pFile);
+                }
+                CONFIG[actsection][actname]=loading;
+            }
+        } while (c != EOF);
+        fclose (pFile);
+    }
+
+    return 0;
+
+}
+
+string load_line() {
+
+    string s;
+    char c=getchar_unlocked();
+    while( c>=32 && c<=126 ) {
+        s+=c;
+        c=getchar_unlocked();
+    }
+    return s;
+
+}
+
+string load_word() {
+
+    string s;
+    char c=getchar_unlocked();
+    while( c>=32 && c<=126 && c!=' ' ) {
+        if(c==EOF)return "EOF";
+        s+=c;
+        c=getchar_unlocked();
+    }
+    return s;
+}
+
+static void retrieve_data_and_print (ResultSet *rs, int colidx) {
+
+    printf("NumberOfRows %d\n",(int)rs -> rowsCount());
+    while (rs->next()) {
+        cout << rs -> getString(colidx) << endl;
+    }
+
+    cout << endl;
+
+}
+
+bool are_strings_equal(string s1, string s2) {
+
+    if(s1.compare(s2)==0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void execute_mysql_request() {
+
+    try {
+        driver = get_driver_instance();
+        con = driver -> connect(CONFIG["mysql"]["dbhost"], CONFIG["mysql"]["user"], CONFIG["mysql"]["password"]);
+        con -> setAutoCommit(1);
+        con -> setSchema(CONFIG["mysql"]["database"]);
+        stmt = con -> createStatement();
+        string Query;
+        Query = load_line();
+        ResultSet *res;
+        res = stmt -> executeQuery (Query.c_str());
+        retrieve_data_and_print (res, 1);
+        printf("EndOfResponse\n");//make sure, that there is nothing like that
+    } catch (SQLException &e) {
+        cout << "ERROR: SQLException in " << __FILE__;
+        cout << " (" << __func__<< ") on line " << __LINE__ << endl;
+        cout << "ERROR: " << e.what();
+        cout << " (MySQL error code: " << e .getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << ")" << endl;
+
+        if (e.getErrorCode() == 1047) {
+            /*
+            Error: 1047 SQLSTATE: 08S01 (ER_UNKNOWN_COM_ERROR)
+            Message: Unknown command
+            */
+            cout << "\nYour server does not seem to support Prepared Statements at all. ";
+            cout << "Perhaps MYSQL < 4.1?" << endl;
+        }
+
+        return EXIT_FAILURE;
+    } catch (std::runtime_error &e) {
+
+        cout << "ERROR: runtime_error in " << __FILE__;
+        cout << " (" << __func__ << ") on line " << __LINE__ << endl;
+        cout << "ERROR: " << e.what() << endl;
+
+        return EXIT_FAILURE;
     }
 
 }
+
+void send_file_out(string path) {
+    char buffer[256];
+    FILE *file;
+    if( (fp=fopen(path,"r")) == NULL ) {
+        fprintf( stderr, "Could not open file \"%s\"\n", file );
+        return ;
+    }
+    while( fgets(buffer,sizeof buffer,file) ) {
+        printf( "%s", buffer );
+    }
+    fclose( file );
+}
+
+int save_bytes_to_file(int size, string path) {
+
+    /*ommit header*/
+    /*load bytes to file*/
+    /*ommit footer*/
+    return 0;
+}
+
+//file handling
+void download_file() {
+
+    int size;
+    string name,tmpfile;
+    string tmppath=CONFIG["paths"]["tmp"]+get_random_string(16);
+    name=load_word();
+    size=atoi(load_word().c_str());
+    int download=save_bytes_to_file(size,tmppath);
+    if(download==0) {
+        //nothing happend
+        //move file to other files
+        printf("OK\n");
+    } else {
+        printf("\nSomething went wrong downloading file! ERRNO: %d\n",download);
+    }
+
+}
+
+void send_file() {
+
+    string name;
+    name=load_word();
+    send_file_out(CONFIG["paths"]["files"]+name);
+
+}
+
+//7zip handling
+void download_7z() {
+
+}
+
+void send_7z() {
+
+}
+
+void load_file_to_7z() {
+
+}
+
+string get_random_string(int len) {
+
+    string s;
+    srand(time(NULL));
+    char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < len; ++i) {
+        s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+
+    return s;
+
+}
+
